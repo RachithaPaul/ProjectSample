4dddf4830b3559d7152a41bed88b32c73e74ee77
RachithaPaul <rachitha.paul@outlook.com>
2015-07-15 11:15:05 -0400

    Update DABasic.cs

--- DataAccessLayer/DABasic.cs
+++ DataAccessLayer/DABasic.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Data.OleDb;
+//using System.Web;
+//using Excel = Microsoft.Office.Interop.Excel; 
 namespace DataAccessLayer
 {
     public class DABasic
@@ -23,7 +25,20 @@ namespace DataAccessLayer
         {
             strConn = "Data Source=RACHITHA-PC\\SQLEXPRESS;Initial Catalog=CodeSample;Integrated Security=True";
             SqlConnection con=new SqlConnection(strConn);
+            con.Open();
             return con;
+        }
+        public OleDbConnection getExcelConnection()
+        {
+          //  string excel = HttpContext.Current.Server.MapPath("App_Data\\Address.xlsx");
+            // strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+ excel +";Extended Properties=Excel 12.0 Xml";
+           strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\DataFolderRachitha\\ASP.NET\\ProjectSample\\ProjectSample\\App_Data\\Address.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;'";
+
+            OleDbConnection Ocon = new OleDbConnection(strConn);
+            Ocon.Open();
+            return Ocon;
+
         }
     }
 }
+ 
